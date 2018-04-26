b077546c4dcfb35345bce466a5fe30c8e0a8aac0
mariesonko <mariesonko62@gmail.com>
2018-04-25 23:39:55 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+
+attr_accessor :name, :meow
+  def initialize
+    @name = name
+    @meow =meow
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def meow
+  puts   "meow!"
+  end
+end
